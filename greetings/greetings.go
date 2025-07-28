a05676b9256dfe306478ebe2fc7c295b2c972f92
Mustafa Aksu <mustafaksu4@gmail.com>
2025-07-28 21:23:03 +0200

    [ + ] Add new func

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -6,7 +6,7 @@ import (
 	"math/rand"
 )
 
-func Hello(name string) (string, error) { 
+func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
@@ -14,11 +14,11 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
-func Hellos(names []string) (map[string]string, error){
+func Hellos(names []string) (map[string]string, error) {
 
 	messages := make(map[string]string)
 
-	for _, name:= range names {
+	for _, name := range names {
 		message, err := Hello(name)
 		if err != nil {
 			return nil, err
@@ -31,8 +31,8 @@ func Hellos(names []string) (map[string]string, error){
 func randomFormat() string {
 	formats := []string{
 		"Hi, %v. Welcome!",
-        "Great to see you, %v!",
-        "Hail, %v! Well met!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
 	}
 
 	return formats[rand.Intn(len(formats))]
